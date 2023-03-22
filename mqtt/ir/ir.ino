102ad48ae6c81ceb988ee00669e5f850d3ecc471
Samarth Mistry <g33ky.sam@gmail.com>
2023-03-22 15:46:16 +0530

    Update ir.ino

--- mqtt/ir/ir.ino
+++ mqtt/ir/ir.ino
@@ -1,19 +1,77 @@
+
+
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h>
+
+#include "arduino_secrets.h"
 int irSensor = 9;
-int led = 13;
+int led = 12;
 int previousState = -1;
 
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "broker.hivemq.com";
+int        port     = 1883;
+const char topic[]  = "/sensorData";
+
+
+
+// int count = 0;
+
 void setup() {
-  Serial.begin(9600);
   pinMode(irSensor, INPUT);
   pinMode(led, OUTPUT);
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
 }
 
 void loop() {
+  mqttClient.poll();
   int readSensor = digitalRead(irSensor);
+
   if (readSensor == 0) {
     digitalWrite(led, HIGH);
     if (previousState != readSensor) {
       Serial.println("Motion detected");
+      mqttClient.beginMessage(topic);
+      mqttClient.print("Motion detected");
+      mqttClient.endMessage();      
       previousState = readSensor;
       delay(500);
     }
@@ -21,6 +79,9 @@ void loop() {
     digitalWrite(led, LOW);
     if (previousState != readSensor) {
       Serial.println("Motion ended");
+      mqttClient.beginMessage(topic);
+      mqttClient.print("Motion ended");
+      mqttClient.endMessage(); 
       previousState = readSensor;
       delay(500);
     }
